0bbcbfeb84151c497782ca890cd441cbf284699d
Jiaxin Pei <pedropei@umich.edu>
2022-01-18 00:40:12 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Potato'
+copyright = '2021, Blablablab'
+author = 'Blablablab'
 
 release = '0.1'
 version = '0.1.0'
