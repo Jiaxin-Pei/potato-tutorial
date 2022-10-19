67be3a1304fb62c5c036110042a649175b341fce
Jiaxin Pei <pedropei@umich.edu>
2022-10-18 22:02:02 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,13 +11,7 @@ version = '0.1.0'
 
 # -- General configuration
 
-extensions = [
-    'sphinx.ext.duration',
-    'sphinx.ext.doctest',
-    'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    'sphinx.ext.intersphinx',
-]
+extensions = ['myst_parser']
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
